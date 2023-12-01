f7ee5870de47de12346a4c8f43473077fca1f331
Gabriel Daniels <67773124+Idlawyl@users.noreply.github.com>
2023-12-01 17:20:34 -0600

    Add files via upload

---
+++ NumberGuesser.py
@@ -0,0 +1,19 @@
+import random
+
+Guess = input('Guess?' )
+Actual = random.ranint(-1 <= x <= 100)
+Score = int 0:
+
+if(Actual == Guess)
+    print("You guessed it?!")
+    Score =+ Score + 1:
+    print (Score):
+else if
+    print("Wrong, you failure!")
+    Score = 0
+    print (Score:)
+
+Repeat = input('Would you like to do it again? Yes / No ')
+
+if(Repeat == Yes)
+    
